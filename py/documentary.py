9eef32c601ed1aa59e76a7bf4c2e64085368b3b5
claudiarom <claudia.romanello@studio.unibo.it>
2025-11-17 11:38:17 +0100

    fix

--- py/documentary.py
+++ py/documentary.py
@@ -5,23 +5,31 @@ from rdflib import Namespace, Graph, RDF, URIRef, OWL, Literal, XSD, RDFS, FOAF
 # NAMESPACES
 
 renzi = Namespace("https://github.com/CineFiles25/informational-science-and-cultural-heritage")
+rdf = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+rdfs = Namespace("http://www.w3.org/2000/01/rdf-schema#")
+owl = Namespace("http://www.w3.org/2002/07/owl#")
 schema = Namespace("https://schema.org/")
 dc = Namespace("http://purl.org/dc/elements/1.1/")
 dcterms = Namespace("http://purl.org/dc/terms/")
 crm = Namespace("http://www.cidoc-crm.org/cidoc-crm/")
 foaf = Namespace("http://xmlns.com/foaf/0.1/")
+fiaf = Namespace("https://fiaf.github.io/film-related-materials/objects/")
 
 # GRAPH CREATION
 
 g = Graph()
 
 nsDict = { 
-    "renzi": renzi,
+    "renzi": renzi,   
+    "rdf": rdf,
+    "rdfs": rdfs,
+    "owl": owl,
     "schema": schema,
     "dc": dc,
     "dcterms": dcterms,
     "crm": crm,
-    "foaf": foaf
+    "foaf": foaf,
+    "fiaf": fiaf
 }
 
 def graph_bindings():
--- py/library.py
+++ py/library.py
@@ -5,23 +5,31 @@ from rdflib import Namespace, Graph, RDF, URIRef, OWL, Literal, XSD, RDFS, FOAF
 # NAMESPACES
 
 renzi = Namespace("https://github.com/CineFiles25/informational-science-and-cultural-heritage")
+rdf = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+rdfs = Namespace("http://www.w3.org/2000/01/rdf-schema#")
+owl = Namespace("http://www.w3.org/2002/07/owl#")
 schema = Namespace("https://schema.org/")
 dc = Namespace("http://purl.org/dc/elements/1.1/")
 dcterms = Namespace("http://purl.org/dc/terms/")
 crm = Namespace("http://www.cidoc-crm.org/cidoc-crm/")
 foaf = Namespace("http://xmlns.com/foaf/0.1/")
+fiaf = Namespace("https://fiaf.github.io/film-related-materials/objects/")
 
 # GRAPH CREATION
 
 g = Graph()
 
 nsDict = { 
-    "renzi": renzi,
+    "renzi": renzi,   
+    "rdf": rdf,
+    "rdfs": rdfs,
+    "owl": owl,
     "schema": schema,
     "dc": dc,
     "dcterms": dcterms,
     "crm": crm,
-    "foaf": foaf
+    "foaf": foaf,
+    "fiaf": fiaf
 }
 
 def graph_bindings():
@@ -34,7 +42,7 @@ def graph_bindings():
 library = URIRef(renzi + "renziLibrary")
 renzoRenzi = URIRef(renzi + "renzoRenzi")
 
-# g.add((renzoRenzi, OWL.sameAs, URIRef("http://viaf.org/viaf/40486517")))
+# g.add((renzoRenzi, OWL.sameAs, URIRef("http://viaf.org/viaf/40486517"))) ?
 
 # MAPPING TO ONTOLOGIES 
 
@@ -43,7 +51,7 @@ renziLibrary = pd.read_csv("../csv/library.csv", keep_default_na=False, encoding
 g = graph_bindings()
 
 for _, row in renziLibrary.iterrows():
-    # g.add((library, RDF.type, URIRef(schema + "Library")))
+    g.add((library, RDF.type, URIRef(schema + "Library")))
     # g.add((library, RDF.type, URIRef(foaf + "Organization")))
     g.add((library, OWL.sameAs, URIRef("https://isni.org/isni/0000000459141457")))
     g.add((library, dc.identifier, Literal(row["Id ISIL"])))
@@ -69,4 +77,4 @@ for _, row in renziLibrary.iterrows():
 
 g.serialize(format="turtle", destination="../ttl/renziLibrary.ttl") 
 
-print("CSV converted to RDF/XML!")
+print("CSV converted to TTL!")
