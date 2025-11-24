a72296c08de3e963e54731660f238658955479cf
claudiarom <claudia.romanello@studio.unibo.it>
2025-11-24 12:50:16 +0100

    fix

---
+++ scripts/graph_creation.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from pandas import read_csv
+from rdflib import Namespace, Graph, RDF, URIRef, OWL, Literal, XSD, RDFS, FOAF
+
+# NAMESPACES
+
+rrr = Namespace("https://github.com/CineFiles25/TheRevolussionOfRenzoRenzi/")
+rdf = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+rdfs = Namespace("http://www.w3.org/2000/01/rdf-schema#")
+owl = Namespace("http://www.w3.org/2002/07/owl#")
+schema = Namespace("https://schema.org/")
+dc = Namespace("http://purl.org/dc/elements/1.1/")
+dcterms = Namespace("http://purl.org/dc/terms/")
+dbo = Namespace("http://dbpedia.org/ontology/")
+crm = Namespace("http://www.cidoc-crm.org/cidoc-crm/")
+foaf = Namespace("http://xmlns.com/foaf/0.1/")
+fiaf = Namespace("https://fiaf.github.io/film-related-materials/objects/")
+
+# GRAPH CREATION
+
+g = Graph()
+
+ns_dict = { 
+    "rrr": rrr,   
+    "rdf": rdf,
+    "rdfs": rdfs,
+    "owl": owl,
+    "schema": schema,
+    "dc": dc,
+    "dcterms": dcterms,
+    "dbo": dbo,
+    "crm": crm,
+    "foaf": foaf,
+    "fiaf": fiaf
+}
+
+def graph_bindings():
+    for prefix, ns in ns_dict.items():
+        g.bind(prefix, ns)
+    return g
+
+# SERIALIZATION
+
+g.serialize(format="turtle", destination="output_path.ttl")
+
+print("CSV converted to TTL!")
