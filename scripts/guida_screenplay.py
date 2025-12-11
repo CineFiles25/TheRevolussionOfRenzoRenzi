f2f33e2e9791f6ea6e18c28010c4bace9cf6cd8c
claudiarom <claudia.romanello@studio.unibo.it>
2025-12-11 12:30:23 +0100

    added py script screenplay

---
+++ scripts/guida_screenplay.py
@@ -0,0 +1,85 @@
+from pandas import read_csv
+from rdflib import Namespace, Graph, RDF, URIRef, OWL, Literal, XSD, RDFS
+
+# NAMESPACES
+
+rrr = Namespace("https://github.com/CineFiles25/TheRevolussionOfRenzoRenzi/")
+rdf = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+rdfs = Namespace("http://www.w3.org/2000/01/rdf-schema#")
+owl = Namespace("http://www.w3.org/2002/07/owl#")
+schema = Namespace("https://schema.org/")
+dc = Namespace("http://purl.org/dc/elements/1.1/")
+dcterms = Namespace("http://purl.org/dc/terms/")
+dbo = Namespace("http://dbpedia.org/ontology/")
+crm = Namespace("http://www.cidoc-crm.org/cidoc-crm/")
+foaf = Namespace("http://xmlns.com/foaf/0.1/")
+fiaf = Namespace("https://fiaf.github.io/film-related-materials/objects/")
+skos = Namespace("http://www.w3.org/2004/02/skos/core#")
+
+# GRAPH CREATION
+
+g = Graph()
+
+ns_dict = { 
+    "rrr": rrr,   
+    "rdf": rdf,
+    "rdfs": rdfs,
+    "owl": owl,
+    "schema": schema,
+    "dc": dc,
+    "dcterms": dcterms,
+    "dbo": dbo,
+    "crm": crm,
+    "foaf": foaf,
+    "fiaf": fiaf,
+    "skos": skos
+}
+
+def graph_bindings():
+    for prefix, ns in ns_dict.items():
+        g.bind(prefix, ns)
+    return g
+
+# ENTITIES
+
+guida_screenplay = URIRef(rrr + "guida_per_camminare_all_ombra")
+renzo_renzi = URIRef(rrr + "renzo_renzi")
+cineteca_di_bologna = URIRef(rrr + "cineteca_di_bologna")
+renzi_collection = URIRef(rrr + "renzo_renzi_collection")
+renzi_library = URIRef(rrr + "renzo_renzi_library")
+
+g.add((renzo_renzi, OWL.sameAs, URIRef("http://viaf.org/viaf/40486517")))
+g.add((cineteca_di_bologna, OWL.sameAs, URIRef("http://viaf.org/viaf/124960346")))
+
+# MAPPING
+
+guida_per_camminare_all_ombra = read_csv("../csv/guida_screenplay.csv", keep_default_na=False, encoding="utf-8")
+
+g = graph_bindings()
+
+for idx, row in guida_per_camminare_all_ombra.iterrows():
+    g.add((guida_screenplay, RDF.type, schema.Manuscript))
+    g.add((schema.Manuscript, RDFS.subClassOf, schema.CreativeWork))
+    g.add((guida_screenplay, dcterms.title, Literal(row["title"])))
+    g.add((guida_screenplay, dcterms.alternative, Literal(row["other_title_information"])))
+    g.add((guida_screenplay, dcterms.created, Literal(row["date"], datatype=XSD.gYearMonth)))
+    g.add((guida_screenplay, dcterms.description, Literal(row["level_of_description"])))
+    g.add((guida_screenplay, dcterms.extent, Literal(row["extent"])))
+    g.add((guida_screenplay, dcterms.medium, Literal(row["medium"])))
+    g.add((guida_screenplay, dbo.writer, renzo_renzi))   
+    g.add((guida_screenplay, dcterms.provenance, Literal(row["archival_description"])))
+    g.add((guida_screenplay, crm.P52_has_current_owner, Literal(row["owner"])))
+    g.add((guida_screenplay, dcterms.isPartOf, renzi_collection))
+    g.add((guida_screenplay, dcterms.spatial, renzi_library))
+    g.add((guida_screenplay, crm.P50_has_current_keeper, cineteca_di_bologna))
+    g.add((guida_screenplay, dcterms.description, Literal(row["scope"])))
+    g.add((guida_screenplay, dcterms.description, Literal(row["content"])))
+    g.add((guida_screenplay, dcterms.language, Literal(row["language"], datatype=XSD.language)))    
+    g.add((guida_screenplay, dcterms.relation, Literal(row["related_works"])))
+    g.add((guida_screenplay, dcterms.rights, Literal(row["rights"])))
+
+# SERIALIZATION
+
+g.serialize(format="turtle", destination="../ttl/guida_screenplay.ttl")
+
+print("CSV converted to TTL!")
