1a4d5dff3595c8d7b2499b3edb4795509f129310
Laura Bortoli <laura.bortoli@studio.unibo.it>
2026-03-16 20:00:02 +0100

    Update renzi_library.py

--- scripts/renzi_library.py
+++ scripts/renzi_library.py
@@ -1,103 +1,132 @@
 from pandas import read_csv
-from rdflib import Namespace, Graph, RDF, URIRef, OWL, Literal, XSD, RDFS, FOAF
+from rdflib import Namespace, Graph, RDF, URIRef, Literal, XSD
 
 # NAMESPACES
 
 rrr = Namespace("https://github.com/CineFiles25/TheRevolussionOfRenzoRenzi/")
-rdf = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
-rdfs = Namespace("http://www.w3.org/2000/01/rdf-schema#")
-owl = Namespace("http://www.w3.org/2002/07/owl#")
 schema = Namespace("https://schema.org/")
-dc = Namespace("http://purl.org/dc/elements/1.1/")
 dcterms = Namespace("http://purl.org/dc/terms/")
-dbo = Namespace("http://dbpedia.org/ontology/")
+dc = Namespace("http://purl.org/dc/elements/1.1/")
 crm = Namespace("http://www.cidoc-crm.org/cidoc-crm/")
-foaf = Namespace("http://xmlns.com/foaf/0.1/")
-fiaf = Namespace("https://fiaf.github.io/film-related-materials/objects/")
-skos = Namespace("http://www.w3.org/2004/02/skos/core#")
 
-# GRAPH CREATION
-
+# GRAPH
 g = Graph()
 
-ns_dict = { 
-    "rrr": rrr,   
-    "rdf": rdf,
-    "rdfs": rdfs,
-    "owl": owl,
-    "schema": schema,
-    "dc": dc,
-    "dcterms": dcterms,
-    "dbo": dbo,
-    "crm": crm,
-    "foaf": foaf,
-    "fiaf": fiaf,
-    "skos": skos
-}
+# Bindings
+g.bind("rrr", rrr)
+g.bind("schema", schema)
+g.bind("dcterms", dcterms)
+g.bind("dc", dc)
+g.bind("crm", crm)
 
-def graph_bindings():
-    for prefix, ns in ns_dict.items():
-        g.bind(prefix, ns)
-    return g
-
-g = graph_bindings()
-
-# ENTITIES 
-
-renzi_library = URIRef(rrr + "renzo_renzi_library")
-renzo_renzi = URIRef(rrr + "renzo_renzi")
-cineteca_di_bologna = URIRef(rrr + "cineteca_di_bologna")
+# ENTITIES
+library = URIRef(rrr + "renzo_renzi_library")
+renzi = URIRef(rrr + "renzo_renzi")
+cineteca = URIRef(rrr + "cineteca_di_bologna")
 bologna = URIRef(rrr + "bologna")
 renzi_collection = URIRef(rrr + "renzi_collection")
 guida_screenplay = URIRef(rrr + "guida_per_camminare_all_ombra")
 book_il_primo_fellini = URIRef(rrr + "book_il_primo_fellini")
 
-# SAMEAS
+# LOAD CSV
+df = read_csv("../csv/renzi_library.csv", keep_default_na=False, encoding="utf-8")
 
-g.add((renzo_renzi, OWL.sameAs, URIRef("http://viaf.org/viaf/40486517")))
-g.add((cineteca_di_bologna, OWL.sameAs, URIRef("http://viaf.org/viaf/124960346")))
-g.add((bologna, OWL.sameAs, URIRef("http://viaf.org/viaf/257723025")))
+# RESOURCE TYPE
+g.add((library, RDF.type, schema.Library))
 
-# MAPPING 
+# CSV → RDF MAPPING
+for _, row in df.iterrows():
 
-renzo_renzi_library = read_csv("../csv/renzi_library.csv", keep_default_na=False, encoding="utf-8")
+    # Identifier (ISIL)
+    if row.get("id_isil"):
+        g.add((library, dcterms.identifier, Literal(row["id_isil"])))
 
-for idx, row in renzo_renzi_library.iterrows():
-    g.add((renzi_library, RDF.type, schema.Library))
-    g.add((schema.Library, RDFS.subClassOf, schema.LocalBusiness))
-    g.add((renzi_library, OWL.sameAs, URIRef("https://anagrafe.iccu.sbn.it/isil/IT-BO0286")))
-    g.add((renzi_library, dc.identifier, Literal(row["id_isil"])))
-    g.add((renzi_library, schema.name, Literal(row["name"])))
-    g.add((renzi_library, schema.alternateName, Literal(row["alt_title"])))
-    g.add((renzi_library, schema.additionalType, Literal(row["original_function"])))
-    g.add((renzi_library, schema.openingHours, Literal(row["opening_hours"])))
-    g.add((renzi_library, crm.P52_has_current_owner,cineteca_di_bologna))
-    g.add((renzi_library, schema.date, Literal(row["completion_year"], datatype=XSD.gYear)))
-    g.add((renzi_library, schema.foundingDate, Literal(row["foundation_year"], datatype=XSD.gYear)))
-    g.add((renzi_library, schema.address, Literal(row["address"])))
-    g.add((renzi_library, schema.addressLocality, bologna))
-    g.add((renzi_library, schema.geo, Literal(row["coordinates"])))
-    g.add((renzi_library, schema.url, Literal(row["website"], datatype=XSD.anyURI)))
-    g.add((renzi_library, schema.email, Literal(row["email"], datatype=XSD.anyURI)))
-    g.add((renzi_library, schema.telephone, Literal(row["phone_number"], datatype=XSD.string)))
-    g.add((renzi_library, schema.isAccessibleForFree, Literal(row["accessible"], datatype=XSD.boolean)))
-    g.add((renzi_library, schema.description, Literal(row["structure_type"])))
-    g.add((renzi_library, schema.floorSize, Literal(row["area"], datatype=XSD.string)))
-    g.add((renzi_library, schema.seatingCapacity, Literal(row["seats"], datatype=XSD.integer)))
-    g.add((renzi_library, schema.description, Literal(row["audio_system"])))
-    g.add((renzi_library, schema.description, Literal(row["video_system"])))
-    g.add((renzi_library, dbo.dedicatedTo, renzo_renzi))
-    g.add((guida_screenplay, schema.location, renzi_library))
-    g.add((book_il_primo_fellini, schema.location, renzi_library))
-    g.add((renzi_collection, schema.location, renzi_library))
-    g.add((renzi_collection, rdf.type, schema.Collection))
-    g.add((renzi_collection, crm.P52_has_current_owner, cineteca_di_bologna))
-    
-   
+    # Name and alternative name
+    if row.get("name"):
+        g.add((library, schema.name, Literal(row["name"])))
+
+    if row.get("alt_title"):
+        g.add((library, schema.alternateName, Literal(row["alt_title"])))
+
+    # Original function (literal)
+    if row.get("original_function"):
+        g.add((library, dcterms.description, Literal(row["original_function"])))
+
+    # Opening hours
+    if row.get("opening_hours"):
+        g.add((library, schema.openingHours, Literal(row["opening_hours"])))
+
+    # Ownership
+    g.add((library, crm.P52_has_current_owner, cineteca))
+
+    # Completion year
+    if row.get("completion_year"):
+        g.add((library, schema.dateCreated, Literal(row["completion_year"], datatype=XSD.gYear)))
+
+    # Foundation year
+    if row.get("foundation_year"):
+        g.add((library, schema.foundingDate, Literal(row["foundation_year"], datatype=XSD.gYear)))
+
+    # Address
+    if row.get("address"):
+        g.add((library, schema.address, Literal(row["address"])))
+
+    # City (resource)
+    g.add((library, schema.addressLocality, bologna))
+
+    # Coordinates
+    if row.get("coordinates"):
+        g.add((library, schema.geo, Literal(row["coordinates"])))
+
+    # Website
+    if row.get("website"):
+        g.add((library, schema.url, Literal(row["website"])))
+
+    # Email
+    if row.get("email"):
+        g.add((library, schema.email, Literal(row["email"])))
+
+    # Phone
+    if row.get("phone_number"):
+        g.add((library, schema.telephone, Literal(row["phone_number"])))
+
+    # Accessibility
+    if row.get("accessible"):
+        g.add((library, schema.isAccessibleForFree, Literal(row["accessible"], datatype=XSD.boolean)))
+
+    # Structure type
+    if row.get("structure_type"):
+        g.add((library, dcterms.description, Literal(row["structure_type"])))
+
+    # Area
+    if row.get("area"):
+        g.add((library, schema.floorSize, Literal(row["area"])))
+
+    # Seats
+    if row.get("seats"):
+        g.add((library, schema.seatingCapacity, Literal(row["seats"], datatype=XSD.integer)))
+
+    # Audio system
+    if row.get("audio_system"):
+        g.add((library, dcterms.description, Literal(row["audio_system"])))
+
+    # Video system
+    if row.get("video_system"):
+        g.add((library, dcterms.description, Literal(row["video_system"])))
+
+    # Dedication
+    g.add((library, schema.dedicatedTo, renzi))
+
+    # ITEMS LOCATED IN THE LIBRARY
+    g.add((guida_screenplay, schema.location, library))
+    g.add((book_il_primo_fellini, schema.location, library))
+    g.add((renzi_collection, schema.location, library))
+
+    # Collection metadata
+    g.add((renzi_collection, dcterms.isPartOf, library))
+    g.add((renzi_collection, crm.P52_has_current_owner, cineteca))
 
 
 # SERIALIZATION
-
-g.serialize(format="turtle", destination="../ttl/renzi_library.ttl") 
-
-print("CSV converted to TTL!")
+g.serialize(format="turtle", destination="../ttl/renzi_library.ttl")
+print("renzi_library.ttl generated successfully!")
