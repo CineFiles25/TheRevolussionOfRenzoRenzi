4fc4596d4b5a3bd5e94ec73005d64e30978accc8
Laura Bortoli <laura.bortoli@studio.unibo.it>
2026-03-16 20:01:18 +0100

    Update renzi_portrait.py

--- scripts/renzi_portrait.py
+++ scripts/renzi_portrait.py
@@ -1,89 +1,97 @@
 from pandas import read_csv
-from rdflib import Namespace, Graph, RDF, URIRef, OWL, Literal, XSD, RDFS, FOAF
+from rdflib import Namespace, Graph, RDF, URIRef, Literal, XSD
 
 # NAMESPACES
 
 rrr = Namespace("https://github.com/CineFiles25/TheRevolussionOfRenzoRenzi/")
-rdf = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
-rdfs = Namespace("http://www.w3.org/2000/01/rdf-schema#")
-owl = Namespace("http://www.w3.org/2002/07/owl#")
 schema = Namespace("https://schema.org/")
-dc = Namespace("http://purl.org/dc/elements/1.1/")
 dcterms = Namespace("http://purl.org/dc/terms/")
-dbo = Namespace("http://dbpedia.org/ontology/")
+dc = Namespace("http://purl.org/dc/elements/1.1/")
 crm = Namespace("http://www.cidoc-crm.org/cidoc-crm/")
 foaf = Namespace("http://xmlns.com/foaf/0.1/")
-fiaf = Namespace("https://fiaf.github.io/film-related-materials/objects/")
-skos = Namespace("http://www.w3.org/2004/02/skos/core#")
 
-# GRAPH CREATION
-
+# GRAPH
 g = Graph()
 
-ns_dict = { 
-    "rrr": rrr,   
-    "rdf": rdf,
-    "rdfs": rdfs,
-    "owl": owl,
-    "schema": schema,
-    "dc": dc,
-    "dcterms": dcterms,
-    "dbo": dbo,
-    "crm": crm,
-    "foaf": foaf,
-    "fiaf": fiaf,
-    "skos": skos
-}
+# Bindings
+g.bind("rrr", rrr)
+g.bind("schema", schema)
+g.bind("dcterms", dcterms)
+g.bind("dc", dc)
+g.bind("crm", crm)
+g.bind("foaf", foaf)
 
-def graph_bindings():
-    for prefix, ns in ns_dict.items():
-        g.bind(prefix, ns)
-    return g
-
-g = graph_bindings()
-
-# ENTITIES 
-
-renzi_portrait = URIRef(rrr + "portrait_of_renzo_renzi")
-renzo_renzi = URIRef(rrr + "renzo_renzi")
-cineteca_di_bologna = URIRef(rrr + "cineteca_di_bologna")
+# ENTITIES
+portrait = URIRef(rrr + "portrait_of_renzo_renzi")
+renzi = URIRef(rrr + "renzo_renzi")
+cineteca = URIRef(rrr + "cineteca_di_bologna")
 bologna = URIRef(rrr + "bologna")
-renzi_collection = URIRef(rrr + "renzo_renzi_collection")
+renzi_collection = URIRef(rrr + "renzi_collection")
 renzi_library = URIRef(rrr + "renzo_renzi_library")
 
-# SAMEAS
+# LOAD CSV
+df = read_csv("../csv/renzi_portrait.csv", keep_default_na=False, encoding="utf-8")
 
-g.add((renzo_renzi, OWL.sameAs, URIRef("http://viaf.org/viaf/40486517")))
-g.add((cineteca_di_bologna, OWL.sameAs, URIRef("http://viaf.org/viaf/124960346")))
-g.add((bologna, OWL.sameAs, URIRef("http://viaf.org/viaf/257723025")))
+# RESOURCE TYPE
+g.add((portrait, RDF.type, schema.Photograph))
 
-# MAPPING 
+# CSV → RDF MAPPING
+for _, row in df.iterrows():
 
-portrait_of_renzo_renzi = read_csv("../csv/renzi_portrait.csv", keep_default_na=False, encoding="utf-8")
+    # Title
+    if row.get("title"):
+        g.add((portrait, dcterms.title, Literal(row["title"])))
 
-for idx, row in portrait_of_renzo_renzi.iterrows():
-    g.add((renzi_portrait, RDF.type, schema.Photograph))
-    g.add((schema.Photograph, RDFS.subClassOf, schema.CreativeWork))
-    g.add((renzi_portrait, dc.title, Literal(row["title"])))
-    g.add((renzi_portrait, dcterms.creator, Literal(row["creator"])))
-    g.add((renzi_portrait, FOAF.depicts, renzo_renzi))
-    g.add((renzi_portrait, schema.about, Literal(row["depicted_event"])))
-    g.add((renzi_portrait, schema.color, Literal(row["colour"], datatype=XSD.string)))
-    g.add((renzi_portrait, schema.material, Literal(row["material_technique"])))
-    g.add((renzi_portrait, schema.artform, Literal(row["physical_description"])))
-    g.add((renzi_portrait, crm.P45_consists_of, Literal(row["carrier_type"])))
-    g.add((renzi_portrait, schema.fileFormat, Literal(row["format"])))
-    g.add((renzi_portrait, dcterms.isPartOf, Literal(row["collection"])))
-    g.add((renzi_portrait, crm.P52_has_current_owner, Literal(row["owner"])))
-    g.add((renzi_portrait, dcterms.isPartOf, renzi_collection))
-    g.add((renzi_collection, schema.location, renzi_library))
-    g.add((renzi_collection, rdf.type, schema.Collection))
-    g.add((renzi_collection, crm.P52_has_current_owner, cineteca_di_bologna))
-    g.add((renzi_portrait, crm.P50_has_current_keeper, cineteca_di_bologna))
-    g.add((renzi_portrait, schema.description, Literal(row["notes"])))
-    g.add((renzi_portrait, dcterms.rights, Literal(row["rights"])))
-    
+    # Creator (literal)
+    if row.get("creator"):
+        g.add((portrait, dcterms.creator, Literal(row["creator"])))
+
+    # Depicted person (resource)
+    g.add((portrait, foaf.depicts, renzi))
+
+    # Depicted event (literal)
+    if row.get("depicted_event"):
+        g.add((portrait, dc.subject, Literal(row["depicted_event"])))
+
+    # Color
+    if row.get("colour"):
+        g.add((portrait, schema.color, Literal(row["colour"])))
+
+    # Material / technique
+    if row.get("material_technique"):
+        g.add((portrait, dcterms.medium, Literal(row["material_technique"])))
+
+    # Physical description
+    if row.get("physical_description"):
+        g.add((portrait, dcterms.extent, Literal(row["physical_description"])))
+
+    # Carrier type
+    if row.get("carrier_type"):
+        g.add((portrait, crm.P45_consists_of, Literal(row["carrier_type"])))
+
+    # File format
+    if row.get("format"):
+        g.add((portrait, schema.fileFormat, Literal(row["format"])))
+
+    # Collection (literal description only)
+    if row.get("collection"):
+        g.add((portrait, dcterms.relation, Literal(row["collection"])))
+
+    # Notes
+    if row.get("notes"):
+        g.add((portrait, dcterms.description, Literal(row["notes"])))
+
+    # Rights
+    if row.get("rights"):
+        g.add((portrait, dcterms.rights, Literal(row["rights"])))
+
+    # COLLECTION & LOCATION
+    g.add((portrait, dcterms.isPartOf, renzi_collection))
+    g.add((renzi_collection, dcterms.hasPart, portrait))
+    g.add((portrait, crm.P52_has_current_owner, cineteca))
+    g.add((portrait, schema.location, renzi_library))
+
+
 # SERIALIZATION
-
 g.serialize(format="turtle", destination="../ttl/renzi_portrait.ttl")
-print("CSV converted to TTL!")
+print("renzi_portrait.ttl generated successfully!")
